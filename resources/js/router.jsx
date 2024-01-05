cc9f0c8a50b0d1d7e416edc6e08119e9439dd0a3
WarCrazhed <mariozamora5126@gmail.com>
2024-01-05 00:14:19 -0600

    Creando el Layout de autenticación

--- resources/js/router.jsx
+++ resources/js/router.jsx
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from './src/layouts/Layout';
+import { AuthLayout } from './src/layouts/AuthLayout';
+import { Inicio } from './src/views/Inicio';
+import { Login } from './src/views/Login';
+import { Registro } from './src/views/Registro';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Layout />
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Inicio />
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/auth/login',
+                element: <Login />
+            },
+            {
+                path: '/auth/registro',
+                element: <Registro />
+            }
+        ]
     }
 ]);
 
