3b0288d573157625d1defcd58065dd156232f945
WarCrazhed <mariozamora5126@gmail.com>
2024-01-04 23:57:38 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Instalación
+
+Instalación Laravel Vite
+```
+npm install --save-dev @vitejs/plugin-react
+
+npm install react react-dom
+
+npm install
+
+npm install -D tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
+```
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel(['resources/js/app.jsx']),
+        react(),
+    ],
+
+    server: {
+        hmr: {
+            host: 'localhost'
+        }
+    }
+});
